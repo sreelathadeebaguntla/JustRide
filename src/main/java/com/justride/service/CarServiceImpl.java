90cf1aed6463dd3df3ea877be1a440e5052ef04d
Sreelatha Deebaguntla <sreelathadeebaguntla@gmail.com>
2024-06-02 13:46:14 -0500

    Added Expection Handling to CarServiceImpl.java

--- src/main/java/com/justride/service/CarServiceImpl.java
+++ src/main/java/com/justride/service/CarServiceImpl.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import com.amazonaws.services.kms.model.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.justride.exception.CarNotFoundException;
+import com.justride.exception.CarServiceException;
 import com.justride.model.Car;
 import com.justride.model.response.CarPostResponse;
 import com.justride.model.response.CarResponse;
@@ -19,79 +20,116 @@ import com.justride.model.response.CarsResponse;
 @Component
 public class CarServiceImpl implements CarService {
 
-	private static final Logger logger = LogManager.getLogger(CarServiceImpl.class);
+    private static final Logger logger = LogManager.getLogger(CarServiceImpl.class);
 
-	@Autowired
-	private AmazonDynamoDB amazonDynamoDB;
+    @Autowired
+    private AmazonDynamoDB amazonDynamoDB;
 
-	@Override
-	public CarsResponse getCars() {
-		logger.info("This is info");
-		logger.debug("This is debug");
-		logger.warn("This is warn");
-		logger.error("This is error");
-		DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
-		List<Car> cars = dynamoDBMapper.scan(Car.class, new DynamoDBScanExpression());
-		CarsResponse response = new CarsResponse();
-		response.setCarList(cars);
-		return response;
-	}
+    @Override
+    public CarsResponse getCars() {
+        try {
+            logger.info("Fetching all cars");
+            DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+            List<Car> cars = dynamoDBMapper.scan(Car.class, new DynamoDBScanExpression());
+            CarsResponse response = new CarsResponse();
+            response.setCarList(cars);
+            return response;
+        } catch (Exception e) {
+            logger.error("Error fetching all cars", e);
+            throw new CarServiceException("Unable to fetch cars");
+        }
+    }
 
-	@Override
-	public CarResponse getCarById(String id) {
-		DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
-		Car car = dynamoDBMapper.load(Car.class, id);
-		CarResponse response = new CarResponse();
-		response.setCar(car);
-		return response;
-	}
+    @Override
+    public CarResponse getCarById(String id) {
+        try {
+            logger.info("Fetching car with id: " + id);
+            DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+            Car car = dynamoDBMapper.load(Car.class, id);
+            if (car == null) {
+                throw new CarNotFoundException("Car with id " + id + " not found");
+            }
+            CarResponse response = new CarResponse();
+            response.setCar(car);
+            return response;
+        } catch (CarNotFoundException e) {
+            throw e;
+        } catch (Exception e) {
+            logger.error("Error fetching car by id", e);
+            throw new CarServiceException("Unable to fetch car by id");
+        }
+    }
 
-	@Override
-	public CarPostResponse addCar(Car car) {
-		DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
-		dynamoDBMapper.save(car);
-		CarPostResponse response = new CarPostResponse();
-		response.setCreatedCar(car);
-		return response;
+    @Override
+    public CarPostResponse addCar(Car car) {
+        try {
+            logger.info("Adding new car");
+            DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+            dynamoDBMapper.save(car);
+            CarPostResponse response = new CarPostResponse();
+            response.setCreatedCar(car);
+            return response;
+        } catch (Exception e) {
+            logger.error("Error adding car", e);
+            throw new CarServiceException("Unable to add car");
+        }
+    }
 
-	}
+    @Override
+    public String updateCar(String id, Car car) {
+        try {
+            logger.info("Updating car with id: " + id);
+            DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+            Car existingCar = dynamoDBMapper.load(Car.class, id);
+            if (existingCar != null) {
+                dynamoDBMapper.save(car);
+                return "Car data successfully updated";
+            } else {
+                throw new CarNotFoundException("No car found with id: " + id);
+            }
+        } catch (CarNotFoundException e) {
+            throw e;
+        } catch (Exception e) {
+            logger.error("Error updating car", e);
+            throw new CarServiceException("Unable to update car");
+        }
+    }
 
-	@Override
-	public String updateCar(String id,Car car){
-		DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
-		if(dynamoDBMapper.load(Car.class,id)!=null)
-		{
-			dynamoDBMapper.save(car);
-			return "Car Data Successfully updated";
+    @Override
+    public String deleteCarById(String id) {
+        try {
+            logger.info("Deleting car with id: " + id);
+            DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+            Car car = dynamoDBMapper.load(Car.class, id);
+            if (car != null) {
+                dynamoDBMapper.delete(car);
+                return "Successfully deleted car: " + id;
+            } else {
+                throw new CarNotFoundException("No car exists with id: " + id);
+            }
+        } catch (CarNotFoundException e) {
+            throw e;
+        } catch (Exception e) {
+            logger.error("Error deleting car", e);
+            throw new CarServiceException("Unable to delete car");
+        }
+    }
 
-		}else
-		{
-			throw new NotFoundException("No Car found with Id: "+id);
-		}
-
-	}
-	@Override
-	public String deleteCarById(String id){
-		DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
-		Car car = dynamoDBMapper.load(Car.class, id);
-		if(car !=null){
-			dynamoDBMapper.delete(car);
-			return "Successfully Deleted Car:" + id;
-		}
-		return "No Cars Exists with id: " + id;
-	}
-
-	@Override
-	public String deleteAllCars(){
-		DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
-		List<Car> cars=dynamoDBMapper.scan(Car.class, new DynamoDBScanExpression());
-		if(!cars.isEmpty())
-		{
-			dynamoDBMapper.batchDelete(cars);
-			return "Successfully Deleted All Cars Data";
-		}
-		return "No Cars Data exists";
-
-	}
-
+    @Override
+    public String deleteAllCars() {
+        try {
+            logger.info("Deleting all cars");
+            DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+            List<Car> cars = dynamoDBMapper.scan(Car.class, new DynamoDBScanExpression());
+            if (!cars.isEmpty()) {
+                dynamoDBMapper.batchDelete(cars);
+                return "Successfully deleted all cars data";
+            } else {
+                return "No cars data exists";
+            }
+        } catch (Exception e) {
+            logger.error("Error deleting all cars", e);
+            throw new CarServiceException("Unable to delete all cars");
+        }
+    }
 }
